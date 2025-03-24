d7c29a8c75e822fbbb4e389a47c9d0462772ad52
Ruy Vieira <ruy.vieiraneto@gmail.com>
2025-03-24 10:55:51 -0300

    fix: add `OPENAI_API_KEY`

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+import os
 import sys
 from openai import OpenAI
+import logging
+from dotenv import load_dotenv  # Import dotenv to load environment variables
+
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
 
 
 def transcribe_audio(input_path, output_path):
-    client = OpenAI()
+    load_dotenv()
+
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+
+    if not openai_api_key:
+        logger.error(
+            "OPENAI_API_KEY required environment variable is not set. Please check your environment variables."
+        )
+        raise ValueError(
+            "OPENAI_API_KEY not found. Please set the OPENAI_API_KEY environment variable."
+        )
+
+    client = OpenAI(
+        api_key=openai_api_key,
+    )
 
     with open(input_path, "rb") as audio_file:
         transcription = client.audio.transcriptions.create(
